1a94408c2ff54f9b9da0c3359bcadb974c2facbd
rustonbsd <rustonbsd@mailfence.com>
2026-01-21 16:46:13 +0100

    MTU buffer overflow fix (17 peers are max, we define 16)

--- src/core/server/signed_peers.rs
+++ src/core/server/signed_peers.rs
@@ -41,7 +41,7 @@ impl SignedPeersStore {
     pub fn get_random_peers(&mut self, info_hash: &Id) -> Option<Vec<SignedAnnounce>> {
         if let Some(info_hash_lru) = self.info_hashes.get(info_hash) {
             let size = info_hash_lru.len();
-            let target_size = 20;
+            let target_size = 16;
 
             if size == 0 {
                 return None;
@@ -55,7 +55,7 @@ impl SignedPeersStore {
                 );
             }
 
-            let mut results = Vec::with_capacity(20);
+            let mut results = Vec::with_capacity(16);
 
             let mut chunk = vec![0_u8; info_hash_lru.iter().len() * 4];
             getrandom::fill(chunk.as_mut_slice()).expect("getrandom");
@@ -169,6 +169,6 @@ mod test {
 
         let sample = store.get_random_peers(&info_hash).unwrap();
 
-        assert_eq!(sample.len(), 20);
+        assert_eq!(sample.len(), 17);
     }
 }
